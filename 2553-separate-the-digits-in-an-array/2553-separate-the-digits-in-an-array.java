1d8c4eccea991bc46b46aa30e52f0e267c0e45f0
Ritesh Srivastav <riteshjee890@gmail.com>
2026-08-22 16:09:47 +0530

    Time: 5 ms (83.26%), Space: 46.3 MB (95.54%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -8,8 +8,11 @@ class Solution {
                 l2.add(c%10);
                 c =c/10;
             }
-            Collections.reverse(l2);
-            l1.addAll(l2);
+            // Collections.reverse(l2);
+            // l1.addAll(l2);
+            for(int a =l2.size()-1;a>=0;a--){
+                l1.add(l2.get(a));
+            }
         }
         int[] ans = new int[l1.size()];
         for(int i =0;i<ans.length;i++){
